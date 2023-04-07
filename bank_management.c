c99c94b46e901c8ff8bd0fe147606a89cd69b786
Assaduzzaman <md.assaduzzaman66@gmail.com>
2023-04-08 01:26:22 +0600

    creating repo

---
+++ bank_management.c
@@ -0,0 +1,42 @@
+// 1. main interface
+// 1.1 -log in as a admin
+// 1.2 - log in as a customer
+// 1.3 - about project
+
+// 2. if as a admin
+// 2.1 - create a new adming account
+// 2.2 - create a new customer account
+// 2.3 - delete customer account
+// 2.4 - update customer info
+// 2.5 - show all customer profile
+// 2.6 - customer of the year
+// 2.7 - log out
+
+// 3. if as a cumstomer
+// 3.1 - take a loan (show current balance)
+// 3.2 - withdraw money (show current balance)
+// 3.3 - deposit money (show current balance)
+// 3.4 - show profile
+// 3.5 - log out
+
+// create new account / login / logout
+// add different schemes
+// add loan system
+// update information
+// removing existing account
+// show customer profile
+// calculate after transaction
+// customer of the year feature
+// show the list of exitsting account
+// exit
+// about us
+
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+  printf("hi mom, I'm doing a project bank management system!\n");
+  return 0;
+}
